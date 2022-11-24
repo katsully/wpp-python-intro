8a3e9fa6f1976c6459b421dff5b46e6e69fe4064
Kat Sullivan <Kat.Sullivan@wpp.com>
2022-11-24 14:54:26 +0000

    example for Future Machines

---
+++ Class07-Bots/06_RaspberryPiSpotify.py
@@ -0,0 +1,57 @@
+import spotipy
+import json
+import webbrowser
+import urllib.request
+import spotipy.util as util
+import sys
+
+# open file with keys and set the path to your credentials JSON file
+credentials = "spotify_keys.json"
+with open(credentials, "r") as keys:
+    api_tokens = json.load(keys)
+
+# read the keys and assign each to a variable
+# A redirect URI, or reply URL, is the location where the authorization server sends the user once the app has been 
+# successfully authorized and granted an authorization code or access token.
+client_id = api_tokens["client_id"]
+client_secret = api_tokens["client_secret"]
+redirectURI = api_tokens["redirect"]
+username = api_tokens["username"]
+weather_key = api_tokens["weather_api"]
+
+# copy and paste the url into your browser to see what data you are getting back
+# request = urllib.request.Request("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID="+weather_key)
+# response = urllib.request.urlopen(request)
+
+# weather = json.loads(response.read())
+# forecast = weather["weather"][0]["description"]
+
+forecast = "moderate rain"
+# forecast = sys.argv[1]
+
+
+scope = 'user-read-private user-read-playback-state user-modify-playback-state playlist-modify-public'
+token = util.prompt_for_user_token(username, scope, client_id=client_id,
+                           client_secret=client_secret,
+                           redirect_uri=redirectURI)
+
+# Create Spotify Object
+sp = spotipy.Spotify(auth=token)
+
+# learn about more query strings for spotify here: https://developer.spotify.com/documentation/web-api/reference/#/operations/search
+track_results = sp.search(q=forecast, type='track', limit=50)
+tracks = track_results['tracks']['items']
+
+# URI = uniform resource identifier
+track_selection_list = []
+track_art = []
+
+for song in tracks:
+    track_selection_list.append(song['uri'])
+    track_art.append(song['album']['images'][0]['url'])
+
+my_playlist = sp.user_playlist_create(user=username, name=forecast, public=True,
+                                      description="Songs for the weather")
+results = sp.user_playlist_add_tracks(username, my_playlist['id'], track_selection_list)
+webbrowser.open(my_playlist['external_urls']['spotify'])
+
